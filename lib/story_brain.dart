f859c4342d5e35ccfedcda69e1aa68a19271bcd2
jannegithub <janne.ek1409@gmail.com>
2021-01-18 14:28:29 +0100

    monday180121em

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -2,6 +2,8 @@
 import 'story.dart';
 
 //TODO: Step 5 - Create a new class called StoryBrain.
+
+//TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
 class StoryBrain {
   List<Story> _storyData = [
     Story(
@@ -34,9 +36,51 @@ class StoryBrain {
         choice1: 'Restart',
         choice2: '')
   ];
-}
-//TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
 
+  String getStory() {
+    return _storyData[_storyNumber].storyTitle;
+  }
+
+  String getChoice1() {
+    return _storyData[_storyNumber].choice1;
+  }
+
+  String getChoice2() {
+    return _storyData[_storyNumber].choice2;
+  }
+
+  int _storyNumber = 0;
+
+  void nextStory(int choiceNumber) {
+    if (choiceNumber == 1 && _storyNumber == 0) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 0) {
+      _storyNumber = 1;
+    } else if (choiceNumber == 1 && _storyNumber == 1) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 1) {
+      _storyNumber = 3;
+    } else if (choiceNumber == 1 && _storyNumber == 2) {
+      _storyNumber = 5;
+    } else if (choiceNumber == 2 && _storyNumber == 2) {
+      _storyNumber = 4;
+    } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
+      restart();
+    }
+  }
+
+  void restart() {
+    _storyNumber = 0;
+  }
+
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
